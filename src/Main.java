a91ffdd1ebaaafcdc817979f0468956737a36bfc
Arczi2k8 <artur.rachubi@gmail.com>
2024-09-24 19:02:16 +0200

    pojebane

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,32 @@
-
 public class Main {
-    public static void main(String[] args) {
-
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-
-            System.out.println("i = " + i);
+    public static void sex(){
+        double [] tablicaLiczb = new double [10];
+        for (int i = 0; i < tablicaLiczb.length; i++) {
+            tablicaLiczb[i] = (double) (Math.sqrt(i));
+            System.out.println(tablicaLiczb[i]);
         }
     }
+    public static int wyszukiwanieBinarne(int [] tablica, int liczba){
+
+    }
+    public static void sex2(){
+        int [] tablicaLiczb = new int [40];
+        tablicaLiczb[0] = 0;
+        tablicaLiczb[1] = 1;
+        for (int i = 0; i < tablicaLiczb.length-2; i++) {
+            tablicaLiczb[i+2] = tablicaLiczb[i] + tablicaLiczb[i+1];
+        }
+        for (int i = 0; i < tablicaLiczb.length; i++) {
+            System.out.println(tablicaLiczb[i]);
+        }
+
+
+
+    }
+    public static void main(String[] args) {
+        sex();
+        sex2();
+
+    }
+
 }
